a6f5fb2dcbcc33b5e7a2d4456378082418e4ce94
Admin-dev32 <Admin@mannasnackbars.com>
2025-10-28 20:16:32 -0700

    Delete api/health.js

--- api/health.js
+++
@@ -1,8 +0,0 @@
-// api/health.js
-import { sendJSON, handleOptions } from './_utils/cors';
-
-export default async function handler(req, res) {
-  if (handleOptions(req, res)) return;
-  if (req.method !== 'GET') return sendJSON(res, 405, { ok:false, error:'Method not allowed' });
-  return sendJSON(res, 200, { ok:true, service:'affiliate-api', ts:Date.now() });
-}
